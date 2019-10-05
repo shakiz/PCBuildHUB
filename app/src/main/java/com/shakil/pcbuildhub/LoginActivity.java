8a09d00d44ca1d843c64128300e6f09e46693963
shakiz <activities335@gmail.com>
2019-10-05 14:41:52 +0600

    login activity with custom toolbar.

---
+++ app/src/main/java/com/shakil/pcbuildhub/LoginActivity.java
@@ -0,0 +1,33 @@
+package com.shakil.pcbuildhub;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        init();
+        setSupportActionBar(toolbar);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, WelcomeHomeActivity.class));
+            }
+        });
+    }
+
+    private void init() {
+        toolbar = findViewById(R.id.tool_bar);
+    }
+}
--- app/src/main/java/com/shakil/pcbuildhub/WelcomeHomeActivity.java
+++ app/src/main/java/com/shakil/pcbuildhub/WelcomeHomeActivity.java
@@ -1,13 +1,34 @@
 package com.shakil.pcbuildhub;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class WelcomeHomeActivity extends AppCompatActivity {
+
+    private TextView loginTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_welcome);
+
+        init();
+        bindUIWIthComponents();
+    }
+
+    private void init() {
+        loginTextView = findViewById(R.id.login);
+    }
+
+    private void bindUIWIthComponents(){
+        loginTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(WelcomeHomeActivity.this, LoginActivity.class));
+            }
+        });
     }
 }
