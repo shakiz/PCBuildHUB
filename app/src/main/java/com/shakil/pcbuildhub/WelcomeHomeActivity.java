80da379c528e6e77d5a0ef5130d21496e1147208
shakiz <activities335@gmail.com>
2019-10-05 11:16:52 +0600

    updated the welcome page.

--- app/src/main/java/com/shakil/pcbuildhub/WelcomeHomeActivity.java
+++ app/src/main/java/com/shakil/pcbuildhub/WelcomeHomeActivity.java
@@ -3,11 +3,11 @@ package com.shakil.pcbuildhub;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class WelcomeHomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home_welcome);
     }
 }
