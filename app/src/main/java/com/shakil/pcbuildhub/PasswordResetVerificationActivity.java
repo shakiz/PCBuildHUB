5275c611cc8674a568da7685894b0336eab1cc28
Shakil335 <shakil.py@gmail.com>
2019-10-06 13:13:06 +0600

    password verification ui.

--- app/src/main/java/com/shakil/pcbuildhub/PasswordResetVerificationActivity.java
+++ app/src/main/java/com/shakil/pcbuildhub/PasswordResetVerificationActivity.java
@@ -4,21 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.widget.Button;
 import android.widget.LinearLayout;
 
-public class ForgotPasswordActivity extends AppCompatActivity {
+public class PasswordResetVerificationActivity extends AppCompatActivity {
 
     private LinearLayout linearLayout;
-    private Button send;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_forgot_password);
+        setContentView(R.layout.activity_password_reset_verification);
 
         init();
         Animation a = AnimationUtils.loadAnimation(this, R.anim.fadein);
@@ -28,21 +25,14 @@ public class ForgotPasswordActivity extends AppCompatActivity {
 
     private void init() {
         linearLayout = findViewById(R.id.mainLayout);
-        send = findViewById(R.id.send);
     }
 
     private void bindUIWIthComponents(){
-        send.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(ForgotPasswordActivity.this,PasswordResetVerificationActivity.class));
-            }
-        });
     }
 
     @Override
     public void onBackPressed() {
-        startActivity(new Intent(ForgotPasswordActivity.this,LoginActivity.class));
+        startActivity(new Intent(PasswordResetVerificationActivity.this,ForgotPasswordActivity.class));
         overridePendingTransition(R.anim.fadein,R.anim.fadeout);
     }
 }
