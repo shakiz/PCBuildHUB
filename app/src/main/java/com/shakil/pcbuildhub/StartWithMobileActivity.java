c1b66ae2185258e0bb201e52e4ab428425a15b8d
Shakil335 <shakil.py@gmail.com>
2019-10-07 09:18:36 +0600

    start with mobile ui work.

--- app/src/main/java/com/shakil/pcbuildhub/StartWithMobileActivity.java
+++ app/src/main/java/com/shakil/pcbuildhub/StartWithMobileActivity.java
@@ -1,6 +1,7 @@
 package com.shakil.pcbuildhub;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -8,18 +9,19 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
-public class WelcomeHomeActivity extends AppCompatActivity {
+import com.roger.catloadinglibrary.CatLoadingView;
 
-    private TextView loginTextView;
+public class StartWithMobileActivity extends AppCompatActivity {
+
     private LinearLayout linearLayout;
-    private Button startWithMobileBtn,startWithEmailBtn;
+    private Button startBtn;
+    private CatLoadingView mView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_welcome);
+        setContentView(R.layout.activity_start_with_mobile);
 
         init();
         Animation a = AnimationUtils.loadAnimation(this, R.anim.fadein);
@@ -28,26 +30,23 @@ public class WelcomeHomeActivity extends AppCompatActivity {
     }
 
     private void init() {
-        loginTextView = findViewById(R.id.login);
-        startWithMobileBtn = findViewById(R.id.startWithMobile);
-        startWithEmailBtn = findViewById(R.id.startWithEmail);
         linearLayout = findViewById(R.id.mainLayout);
+        startBtn = findViewById(R.id.start);
+        mView = new CatLoadingView();
     }
 
     private void bindUIWIthComponents(){
-
-        loginTextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(WelcomeHomeActivity.this, LoginActivity.class));
-            }
-        });
-
-        startWithMobileBtn.setOnClickListener(new View.OnClickListener() {
+        startBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(WelcomeHomeActivity.this,StartWithMobileActivity.class));
+                mView.show(getSupportFragmentManager(), "");
             }
         });
+    }
+
+    @Override
+    public void onBackPressed() {
+        startActivity(new Intent(StartWithMobileActivity.this,WelcomeHomeActivity.class));
+        overridePendingTransition(R.anim.fadein,R.anim.fadeout);
     }
 }
