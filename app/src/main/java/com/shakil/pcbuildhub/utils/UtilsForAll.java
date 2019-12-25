eaaabd19b1d815a6a1e0bff4ff9ca2747b3569ee
shakiz <activities335@gmail.com>
2019-12-26 00:38:21 +0600

    input validation and utillforall class added

---
+++ app/src/main/java/com/shakil/pcbuildhub/utils/UtilsForAll.java
@@ -0,0 +1,51 @@
+package com.shakil.pcbuildhub.utils;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import com.shakil.pcbuildhub.R;
+
+public class UtilsForAll {
+
+    private Context context;
+    private View view;
+
+    public UtilsForAll(Context context, View view) {
+        this.context = context;
+        this.view = view;
+    }
+
+    public UtilsForAll(Context context) {
+        this.context = context;
+    }
+
+    public UtilsForAll(View view) {
+        this.view = view;
+    }
+
+    public void exitApp(){
+        Intent exitIntent = new Intent(Intent.ACTION_MAIN);
+        exitIntent.addCategory(Intent.CATEGORY_HOME);
+        exitIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(exitIntent);
+    }
+
+    public void setCustomDesignTextView(int resId){
+        TextView textView = view.findViewById(resId);
+        textView.setTextColor(view.getResources().getColor(R.color.md_blue_grey_800));
+        textView.setSingleLine();
+    }
+
+    public int toIntValue(String value){
+        try{
+            Log.v("shakil",""+Integer.parseInt(value));
+            return Integer.parseInt(value);
+        }
+        catch (Exception e){
+            Log.v("shakil",e.getMessage());
+            return 0;
+        }
+    }
+}
