d53864db0baec18ceeca77e9c3840dbf42139eaa
shakiz <activities335@gmail.com>
2019-12-27 21:24:36 +0600

    login activity updated

--- app/src/main/java/com/shakil/pcbuildhub/activities/onboard/LoginActivity.java
+++ app/src/main/java/com/shakil/pcbuildhub/activities/onboard/LoginActivity.java
@@ -1,50 +1,71 @@
 package com.shakil.pcbuildhub.activities.onboard;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.roger.catloadinglibrary.CatLoadingView;
 import com.shakil.pcbuildhub.R;
+import com.shakil.pcbuildhub.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private TextView forgotPasswordTextView;
-    private LinearLayout linearLayout;
+    private ActivityLoginBinding loginBinding;
+    private CatLoadingView mView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        loginBinding = DataBindingUtil.setContentView(this , R.layout.activity_login);
 
         init();
         Animation a = AnimationUtils.loadAnimation(this, R.anim.fadein);
-        linearLayout.startAnimation(a);
+        loginBinding.mainLayout.startAnimation(a);
         bindUIWithComponents();
     }
 
     private void bindUIWithComponents() {
-        forgotPasswordTextView.setOnClickListener(new View.OnClickListener() {
+        loginBinding.forgotPassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(LoginActivity.this,ForgotPasswordActivity.class));
             }
         });
+
+        loginBinding.Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showCustomDialog();
+            }
+        });
     }
 
     private void init() {
-        forgotPasswordTextView = findViewById(R.id.forgotPassword);
-        linearLayout = findViewById(R.id.mainLayout);
+        mView = new CatLoadingView();
     }
 
     @Override
     public void onBackPressed() {
         startActivity(new Intent(LoginActivity.this,WelcomeHomeActivity.class));
         overridePendingTransition(R.anim.fadein,R.anim.push_up_out);
+    }
+
+    public void showCustomDialog() {
+        mView.show(getSupportFragmentManager(), "");
+        mView.setCanceledOnTouchOutside(true);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+            }
+        }, 1400);
     }
 }
